b21fb13dbe6b13b61adb6fd474cc66c22a2f39b1
chunzhi <60135925+chunzhimoe@users.noreply.github.com>
2023-10-31 20:17:22 +0800

    Update download.sh

--- guofeng-template/models/download.sh
+++ guofeng-template/models/download.sh
@@ -1,3 +1,2 @@
-wget -O ./Lora/Moxin_1010.safetensors https://civitai.com/api/download/models/14856
-wget -O ./basemodel.safetensors https://civitai.com/api/download/models/12378
-wget -O ./Lora/Moxin_Shukezouma.safetensors https://civitai.com/api/download/models/16075
+wget -O ./Lora/Moxin_1010.safetensors https://civitai.com/api/download/models/17518
+wget -O ./basemodel.safetensors https://civitai.com/api/download/models/106289
