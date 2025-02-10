9b42fd10db6663d8bc25d1aadea44f7b13467cbd
zhizhi <60135925+chunzhimoe@users.noreply.github.com>
2025-02-10 22:00:43 +0800

    Update start.sh

--- chilloutmix-template/start.sh
+++ chilloutmix-template/start.sh
@@ -3,30 +3,9 @@ echo "Container Started"
 export PYTHONUNBUFFERED=1
 source /workspace/stable-diffusion-webui/venv/bin/activate
 cd /workspace/stable-diffusion-webui
-python relauncher.py &
+echo "starting api"
+python webui.py --port 3000 --nowebui --api --xformers --enable-insecure-extension-access --ckpt ./models/Stable-diffusion/basemodel.safetensors &
+cd /
 
-if [[ $PUBLIC_KEY ]]
-then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-    cd ~/.ssh
-    echo $PUBLIC_KEY >> authorized_keys
-    chmod 700 -R ~/.ssh
-    cd /
-    service ssh start
-    echo "SSH Service Started"
-fi
-
-if [[ $JUPYTER_PASSWORD ]]
-then
-    ln -sf /examples /workspace
-    ln -sf /root/welcome.ipynb /workspace
-
-    cd /
-    jupyter lab --allow-root --no-browser --port=8888 --ip=* \
-        --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' \
-        --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
-    echo "Jupyter Lab Started"
-fi
-
-sleep infinity
+echo "starting worker"
+python -u handler.py
